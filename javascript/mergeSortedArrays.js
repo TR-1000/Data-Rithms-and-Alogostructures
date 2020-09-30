c8e3d14b3e132040ea5f0a428a66eac8addd2e7d
TR-1000 <t.l.ross@outlook.com>
2020-09-30 17:06:03 -0500

    moving files

---
+++ javascript/mergeSortedArrays.js
@@ -0,0 +1,33 @@
+
+function mergeSortedArrays(arr1, arr2) {
+
+  let mergeSortedArray = [];
+  arr2Index = 0;
+  
+  for (var i = 0; i < arr1.length; i++) {
+    if ((arr1[i] < arr2[arr2Index]) || !arr2[arr2Index]) {
+      mergeSortedArray.push(arr1[i]);
+      console.log("pushing "+ arr1[i] + " at the top of for loop");
+    } else {
+      do {
+        mergeSortedArray.push(arr2[arr2Index]);
+        console.log("pushing "+ arr2[arr2Index] + " do..");
+        arr2Index++;
+      } while (arr1[i] >= arr2[arr2Index]);
+      mergeSortedArray.push(arr1[i]);
+      console.log("pushing "+ arr1[i] + " after while loop");
+    }
+  }
+
+  if (arr2[arr2Index]) {
+    mergeSortedArray = mergeSortedArray.concat(arr2.slice(arr2Index))
+  }
+
+  console.log(mergeSortedArray);
+
+}
+
+
+
+
+mergeSortedArrays([4,6,30,31,35,67,100,300], [4,21,23,24,25,30,200])
---
+++ javascript/reverseString.js
@@ -0,0 +1,17 @@
+let myString = "happy";
+
+function reverseString(str) {
+  // Without cheating
+  // let rev = "";
+  // for (i=str.length-1; i>=0; i--) {
+  //   rev += str[i];
+  // }
+  // return(rev);
+
+  return str.split("").reverse().join("");
+
+}
+
+
+
+console.log(reverseString("hi"));
