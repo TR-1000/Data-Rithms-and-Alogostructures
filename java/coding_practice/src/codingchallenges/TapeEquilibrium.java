2f3382dbb34ff2c5808b122bff63784d9b188035
TR-1000 <t.l.ross@outlook.com>
2021-02-04 16:29:31 -0600

    made TapeEquilibrium run faster, but not fast enough

--- java/coding_practice/src/codingchallenges/TapeEquilibrium.java
+++ java/coding_practice/src/codingchallenges/TapeEquilibrium.java
@@ -1,47 +1,68 @@
 package codingchallenges;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class TapeEquilibrium {
 	public static int solution(int[] A) {
-		// very slow function
-		
-		if(A.length<2)
-            return 1;
-		
-		int minDiff = 1;
-		int P = 1; //split position
-		int firstSum = 0;
-		int secondSum = 0;
+		int minDiff = 0;
+		int leftSum = 0; //first sub array after split
+		int sum = Arrays.stream(A).sum();
+		//System.out.println("sum " + sum);
 		List<Integer> intList = new ArrayList<>();
 		
-		while(P < A.length) {
+		for (int i = 0; i < A.length-1; i++) {
+			leftSum += A[i];
+			int rightSum = sum - leftSum;
+			intList.add(Math.abs(leftSum - rightSum));
 			
-			for (int i = 0; i < A.length; i++) {
-				if (i < P) {
-					firstSum += A[i];
-				} else {
-					secondSum += A[i];
-				}
-			}
-			
-			System.out.println("first sum: "+firstSum);
-			System.out.println("second sum: "+secondSum);
-			P++;
-			intList.add(Math.abs(firstSum-secondSum));
-			firstSum=0;
-			secondSum=0;
 		}
 		
 		minDiff = Collections.min(intList);
+		System.out.print("differences ");
+		System.out.println(intList);
 		
 		return minDiff;
+		
+		
+//		// very slow function: 53% score
+//		if(A.length<2)
+//            return 1;
+//		
+//		int minDiff = 1;
+//		int P = 1; //split position
+//		int firstSum = 0;
+//		int secondSum = 0;
+//		List<Integer> intList = new ArrayList<>();
+//		
+//		while(P < A.length) {
+//			
+//			for (int i = 0; i < A.length; i++) {
+//				if (i < P) {
+//					firstSum += A[i];
+//				} else {
+//					secondSum += A[i];
+//				}
+//			}
+//			
+//			System.out.println("first sum: "+firstSum);
+//			System.out.println("second sum: "+secondSum);
+//			P++;
+//			intList.add(Math.abs(firstSum-secondSum));
+//			firstSum=0;
+//			secondSum=0;
+//		}	
+//		minDiff = Collections.min(intList);
+//		
+//		System.out.println(intList);
+//		
+//		return minDiff;
 	}
 	public static void main(String[] args) {
 		
-		int[] A = {1,0,1,8,10,11};
+		int[] A = {3,1,2,4,3};
 		System.out.println(solution(A));
 		
 	}
