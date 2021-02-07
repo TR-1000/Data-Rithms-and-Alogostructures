e0039d6adee0e07ec4d7743dda2ca4a154691b3d
TR-1000 <t.l.ross@outlook.com>
2021-02-06 23:28:24 -0600

    updated fibonacci_generator function

--- python/fibonacci_generator.py
+++ python/fibonacci_generator.py
@@ -1,12 +1,15 @@
-# Fibonacci Generator
+'''Fibonacci Generator'''
 
 def fib_generator(num):
   a = 0
   b = 1
-  for i in range(num):
+  for i in range(num+1):
     yield a
     # the value of "a" now changes to "a" + "b" and the value of "b" changes to the value "a" was
     a, b = a + b, a
 
-# for i in fib_generator(1000):
-#   print(i)
+# loop over generator
+def get_fib_array(n):
+    return [i for i in fib_generator(n)]
+# print out array
+print(get_fib_array(6))
